153b0802345619a20fc26f7c068fa2c31a581996
Anujith Nayak <anujithsnayak@gmail.com>
2026-06-02 20:36:52 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pal=""
+        for i in s:
+            if i.isalnum():
+                pal=pal+i.lower()
+        return pal==pal[::-1]
